a99159b91c277361df5ff0e6500b20c02c8ebfa2
Sejal Agarwal <sagarwal29@ndsj.org>
2026-09-07 00:16:47 -0700

    Add exercise and BMI visualization

--- analyzer.py
+++ analyzer.py
@@ -12,9 +12,13 @@ print("Average Exercise:", round(data["Exercise_Hours"].mean(), 2))
 print("Average Heart Rate:", round(data["Heart_Rate"].mean(), 2))
 
 plt.scatter(data["Sleep_Hours"], data["Heart_Rate"])
-
 plt.xlabel("Sleep (hours)")
 plt.ylabel("Heart Rate")
 plt.title("Sleep vs. Heart Rate")
+plt.show()
 
+plt.scatter(data["Exercise_Hours"], data["BMI"])
+plt.xlabel("Exercise (hours)")
+plt.ylabel("BMI")
+plt.title("Exercise vs. BMI")
 plt.show()
